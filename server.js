6133ff82f386022bd6e109fc0d75a75928759338
Rogerio <rmelloli@emeal.nttdata.com>
2023-05-14 21:52:02 -0300

    Implementado environment variables

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import url from "url";
 import path from "path";
 import express from "express";
 import "./src/config/dbConfig.js"
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 
 const currentPath = url.fileURLToPath(import.meta.url);
 const publicDir = path.join(currentPath, "../src/", "views");
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT;
 app.use(express.static(publicDir));
 
 
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,5 +1,11 @@
 import { MongoClient } from "mongodb";
-const connectionString = '';
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
+/// ---- NÃO SUBIR PARA O GITHUB ----///
+const connectionString = process.env.DB_CONNECTION;
+/// ---- NÃO SUBIR PARA O GITHUB ----///
+
 const client = new MongoClient(connectionString);
 
 let mensagens;
